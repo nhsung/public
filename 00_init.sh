bd0b43397dc80c0def30e4abcb735fc2dacaebbb
nhsung <nhsung@gmail.com>
2022-11-20 02:39:33 +0900

    Update 00_init.sh

--- 00_init.sh
+++ 00_init.sh
@@ -1,9 +1,13 @@
-# Package Repository
-sudo amazon-linux-extras install epel
+## fetch run scripts
+wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git.sh,11_inst_pkgs.sh,sample_curl.sh}
+
+
+## Package Repository
+sudo amazon-linux-extras install epel -y
 sudo yum update -y
 sudo yum upgrade -y
 
-# GIT
-sudo yum install git -y
-mkdir ~/GIT
-git init
+
+## run scripts on ec2
+cat 01_inst_git.sh | bash -s
+cat 11_inst_pkgs.sh | bash -s
