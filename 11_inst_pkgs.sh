cc10ce8d118f4644fd98adc6a37b6d2c9e2b2f75
nhsung <nhsung@gmail.com>
2022-11-20 02:04:08 +0900

    Create 11_inst_pkgs.sh

---
+++ 11_inst_pkgs.sh
@@ -0,0 +1,27 @@
+## amazon-linux-extras
+sudo amazon-linux-extras install nginx1
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+
+## yum
+
+# admin
+sudo yum -y install neovim tmux tmate
+
+# performance
+sudo yum -y install stress-ng fio
+
+# monitoring
+sudo yum -y install atop
+sudo systemctl enable atop
+sudo systemctl start atop
+
+sudo yum -y install nmon htop
+
+# network & storage
+sudo yum -y install wireshark nmap hping3 iperf3 \
+prettyping httping lftp \
+ioping
+
+# shell
