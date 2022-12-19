986b793a063f045f4821362c5c898d757a9a8666
nhsung <nhsung@gmail.com>
2022-12-20 05:14:35 +0900

    Update 22_inst_neovim.sh

--- 22_inst_neovim.sh
+++ 22_inst_neovim.sh
@@ -3,18 +3,11 @@ sudo yum install -y cmake
 sudo yum install -y python34-{devel,pip}
 sudo pip3.4 install neovim --upgrade
 
-# cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.20.0-rc3/cmake-3.20.0-rc3.tar.gz
-tar -xvzf cmake-3.20.0-rc3.tar.gz
-cd cmake-3.20.0-rc3
+make install
+yum install gcc-c++
+wget https://cmake.org/files/v3.10/cmake-3.10.0.tar.gz
+tar -xvzf cmake-3.10.0.tar.gz
+cd cmake-3.10.0
 ./bootstrap
 make
 make install
-
-(
-cd "$(mktemp -d)"
-git clone https://github.com/neovim/neovim.git
-cd neovim
-make CMAKE_BUILD_TYPE=Release
-sudo make install
-)
