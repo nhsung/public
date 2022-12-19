52fd06c76ff9b677d045c98e9ff8c883169ca3c1
nhsung <nhsung@gmail.com>
2022-12-20 03:13:43 +0900

    Update 01_inst_git_zsh.sh

--- 01_inst_git_zsh.sh
+++ 01_inst_git_zsh.sh
@@ -5,15 +5,15 @@ git init
 
 #### SHELL
 ## ZSH
-yum -y install util-linux-ng
+yum -y install zsh util-linux-user
 # enable zsh login shell
 sudo chsh -s $(which zsh) ec2-user
 ## OH-MY-ZSH
 sudo su - ec2-user -c "$(\curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ## FONTS
-sudo su - ec2-user -c "git clone https://github.com/powerline/fonts.git --depth=1"
+sudo su - ec2-user -c "git clone https://github.com/powerline/fonts.git --depth=1 && ./fonts/install.sh"
 ## POWERLEVEL10K
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sudo su - ec2-user -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/themes/powerlevel10k"
 ## FZF
-sudo su - ec2-user -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf"
+sudo su - ec2-user -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all"
 sudo su - ec2-user -c "~/.fzf/install --all"
