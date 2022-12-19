6f21376cad732c5974dfe3f73bff7573c3dce531
nhsung <nhsung@gmail.com>
2022-12-20 03:46:06 +0900

    Create 21_inst_tmux.sh

---
+++ 21_inst_tmux.sh
@@ -0,0 +1,19 @@
+# Packages
+sudo yum groupinstall -y "Development Tools"
+sudo yum install -y libevent-devel ncurses-devel
+
+# Compile
+cd /tmp
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure && make
+sudo cp -p ./tmux /usr/local/bin/
+END
+
+chmod 755 ./tmux_install.sh
+./tmux_install.sh
+
+
+# TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
