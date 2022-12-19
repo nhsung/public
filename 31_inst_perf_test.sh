4ca55dcb1baf87f45f90f9b6b877999d32c499b2
nhsung <nhsung@gmail.com>
2022-12-20 03:55:38 +0900

    Update and rename 22_inst_neovimsh to 31_inst_perf_test.sh

---
+++ 31_inst_perf_test.sh
@@ -0,0 +1,14 @@
+sudo su -
+amazon-linux-extras install -y epel
+yum install -y stress-ng
+yum install -y systat atop
+yum install -y iotop
+
+
+## configure the ATOP and SAR monitoring tools
+# https://aws.amazon.com/premiumsupport/knowledge-center/ec2-linux-configure-monitoring-tools/
+# AL2
+sudo sed -i 's/^LOGINTERVAL=600.*/LOGINTERVAL=60/' /etc/sysconfig/atop
+sudo sed -i -e 's|*/10|*/1|' -e 's|every 10 minutes|every 1 minute|' /etc/cron.d/sysstat
+sudo systemctl enable atop.service crond.service sysstat.service
+sudo systemctl restart atop.service crond.service sysstat.service
