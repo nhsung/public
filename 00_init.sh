fc9bf11a66a61d78c7b3ddb6951cf8c24f6dd1a7
nhsung <nhsung@gmail.com>
2022-12-20 03:49:09 +0900

    Update 00_init.sh

--- 00_init.sh
+++ 00_init.sh
@@ -2,7 +2,8 @@
 
 ## fetch run scripts
 cd /tmp
-wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,sample_curl.sh}
+#wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,sample_curl.sh}
+wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,21_inst_tmux.sh,sample_curl.sh}
 
 
 ## Package Repository
