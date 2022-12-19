0a619e03752c9b38864e44eb5fcbde4f1b85b223
nhsung <nhsung@gmail.com>
2022-12-20 03:59:40 +0900

    Update 00_init.sh

--- 00_init.sh
+++ 00_init.sh
@@ -4,7 +4,7 @@
 cd /tmp
 #wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,sample_curl.sh}
 wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,21_inst_tmux.sh,sample_curl.sh}
-wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,21_inst_tmux.sh,22_inst_neovimsh,sample_curl.sh}
+wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,21_inst_tmux.sh,31_inst_perf_test.sh,sample_curl.sh}
 
 
 ## Package Repository
