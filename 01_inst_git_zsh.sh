b3f7fa61a487bf086c6ec343c101a4f934d41188
nhsung <nhsung@gmail.com>
2023-01-06 02:32:38 +0900

    test

--- 01_inst_git_zsh.sh
+++ 01_inst_git_zsh.sh
@@ -19,12 +19,13 @@ sudo su - ec2-user -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~
 sudo su - ec2-user -c "~/.fzf/install --all"
 
 ## PLUGIN
-sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
-sudo su - ec2-user -c "git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
-sudo su - ec2-user -c "git clone https://github.com/wfxr/forgit ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/forgit"
-sudo su - ec2-user -c "git clone https://github.com/pierpo/fzf-docker ~/.oh-my-zsh/plugins/fzf-docker"
+sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-completions              ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions"
+sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-autosuggestions          ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
+sudo su - ec2-user -c "git clone https://github.com/wfxr/forgit                            ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/forgit"
+sudo su - ec2-user -c "git clone https://github.com/pierpo/fzf-docker                      ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-docker"
+
 
 
 ## GIT CLONE
--- 22_inst_neovim.sh
+++ 22_inst_neovim.sh
@@ -6,15 +6,17 @@ sudo su - ec2-user -c "mkdir -p ~/.config/nvim"
 sudo su - ec2-user -c "mkdir -p ~/.local/share/nvim/"
 
 
-# vim-plug
-#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-sudo su - ec2-user -c "nvim +'PlugInstall --sync' +qa"
-
-
+# config
 cd /tmp
 # init.vim
-cp -p ./public/DOTFILES/init.vim ~ec2-user/.config/nvim/
+cp -pfn ./public/DOTFILES/init.vim ~ec2-user/.config/nvim/
 # plugin
 cd ./public/DOTFILES/nvim
 cp -rpfn session shada site ~ec2-user/.local/share/nvim/
+
+
+## vim-plug
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# plugin install
+sudo su - ec2-user -c "nvim +'PlugInstall --sync' +qa"
