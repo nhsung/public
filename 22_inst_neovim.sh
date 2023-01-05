bc745d3ea608692f4401fcc6380d2b8d7ad4c40d
nhsung <nhsung@gmail.com>
2023-01-06 00:50:00 +0900

    test

--- 22_inst_neovim.sh
+++ 22_inst_neovim.sh
@@ -1,13 +1,20 @@
+## neovim
 curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo 
 yum -y install neovim
-
+## create dir
 mkdir ~/.config/nvim
+mkdir ~/.local/share/nvim/
 
+
+# vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 nvim +'PlugInstall --sync' +qa
 
-mkdir ~/.local/share/nvim/
+
+cd /tmp
+# init.vim
+cp -p ./public/DOTFILES/init.vim ~/.config/nvim/
+# plugin
 cd ./public/DOTFILES/nvim
 cp -rp session shada site ~/.local/share/nvim/
