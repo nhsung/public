1e964a7c4f045868bc3a735d1b92bc3d30f50721
nhsung <nhsung@gmail.com>
2023-01-08 15:01:56 +0900

    test

--- 00_init.sh
+++ 00_init.sh
@@ -1,22 +1,22 @@
 #### curl -sH "Cache-Control: no-cache, no-store"  https://raw.githubusercontent.com/nhsung/public/master/00_init.sh | bash -s
 
+# $USER
+export USER=~ec2-user
+
 
 ## TimeZone 
 timedatectl set-timezone Asia/Seoul
 
 
 ## fetch run scripts
-cd /tmp
-#wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,sample_curl.sh}
-#wget https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,21_inst_tmux.sh,sample_curl.sh}
-wget https://raw.githubusercontent.com/nhsung/public/master/\
-{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,21_inst_tmux.sh,22_inst_neovim.sh,31_inst_perf_test.sh,sample_curl.sh}
+mkdir $USER/GIT
+wget -P $USER/GIT https://raw.githubusercontent.com/nhsung/public/master/{01_inst_git_zsh.sh,02_inst_amazon.sh,11_inst_pkgs.sh,21_inst_tmux.sh,22_inst_neovim.sh,sample_curl.sh}
 
 
 ## Package Repository
 amazon-linux-extras enable epel
 amazon-linux-extras install epel -y
-yum -y install epel-release
+yum install epel-release -y
 yum update -y
 yum upgrade -y
 
--- 01_inst_git_zsh.sh
+++ 01_inst_git_zsh.sh
@@ -1,40 +1,40 @@
+# $USER
+export USER=ec2-user
+
+
 ## GIT
-yum -y install git
-mkdir ~/GIT && cd ~/GIT
-git init
+yum install git -y
+
 
 #### SHELL
 ## ZSH
 yum -y install zsh util-linux-user
-## enable zsh login shell
-sudo chsh -s $(which zsh) ec2-user
+## switch to zsh as login shell
+sudo chsh -s $(which zsh) $USER
 ## OH-MY-ZSH
-sudo su - ec2-user -c "$(\curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo su - $USER -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ## POWERLEVEL10K
-sudo su - ec2-user -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/themes/powerlevel10k"
+sudo su - $USER -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/themes/powerlevel10k"
 ## FONTS
-sudo su - ec2-user -c "git clone https://github.com/powerline/fonts.git --depth=1 && ./fonts/install.sh"
+sudo su - $USER -c "git clone https://github.com/powerline/fonts.git --depth=1 && ./fonts/install.sh"
 ## FZF
-sudo su - ec2-user -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all"
-sudo su - ec2-user -c "~/.fzf/install --all"
+sudo su - $USER -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all"
+sudo su - $USER -c "~/.fzf/install --all"
 
 ## PLUGIN
-sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-completions                      ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/plugins/zsh-completions"
-sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-autosuggestions                  ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git          ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-sudo su - ec2-user -c "git clone https://github.com/zsh-users/zsh-history-substring-search         ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
-sudo su - ec2-user -c "git clone https://github.com/wfxr/forgit                                    ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/plugins/forgit"
-sudo su - ec2-user -c "git clone https://github.com/pierpo/fzf-docker                              ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/plugins/fzf-docker"
-sudo su - ec2-user -c "git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~ec2-user/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
-
-
-
+sudo su - $USER -c "git clone https://github.com/zsh-users/zsh-completions                      ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/plugins/zsh-completions"
+sudo su - $USER -c "git clone https://github.com/zsh-users/zsh-autosuggestions                  ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+sudo su - $USER -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git          ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+sudo su - $USER -c "git clone https://github.com/zsh-users/zsh-history-substring-search         ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
+sudo su - $USER -c "git clone https://github.com/wfxr/forgit                                    ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/plugins/forgit"
+sudo su - $USER -c "git clone https://github.com/pierpo/fzf-docker                              ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/plugins/fzf-docker"
+sudo su - $USER -c "git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~$USER/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
 
 
 ## GIT CLONE
-cd /tmp
+cd $USER/GIT
 git clone https://github.com/nhsung/public.git
 cd public/DOTFILES
-chown -R ec2-user:ec2-user .*
-cp -pf .zsh* .fzf*sh .tmate.conf ~ec2-user/
-cp -pf .p10k.zsh .fzf.*sh        ~ec2-user/
+chown -R $USER:$USER .*
+cp -pf .zsh* .fzf*sh .tmate.conf $USER/
+cp -pf .p10k.zsh .fzf.*sh        $USER/
